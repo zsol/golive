2431f3c0d008b8e77ac9f1fcc6ac4213bbfde04c
Zsolt Dollenstein <zsol.zsol@gmail.com>
2016-08-24 16:05:45 +0200

    automatically create fb live video on startup

--- popup.js
+++ popup.js
@@ -1,56 +1,3 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-
 function goOnClick() {
   chrome.desktopCapture.chooseDesktopMedia(
     ["screen", "window", "tab"],
@@ -82,8 +29,63 @@ function goOnClick() {
   );
 }
 
+function wvOnInitialLoad() {
+  let wv = document.getElementById('wv');
+  gotNewUrl(wv.src);
+}
+
+function wvOnRedirect(e) {
+  gotNewUrl(e.newUrl);
+}
+
+function gotNewUrl(url) {
+  let a = document.createElement('a');
+  a.href = url;
+  if (a.hostname !== "www.facebook.com" ||
+      a.pathname !== "/connect/login_success.html") {
+    return;
+  }
+  let wv = document.getElementById('wv');
+  wv.remove();
+
+  code = a.search.slice(6);
+  let req = new XMLHttpRequest();
+  req.addEventListener('load', gotAccessToken);
+  path = "https://graph.facebook.com/v2.7/oauth/access_token";
+  client_id = "?client_id=1072171686192806";
+  redirect_uri = "&redirect_uri=https://www.facebook.com/connect/login_success.html";
+  client_secret = "&client_secret=" + FB_CLIENT_SECRET;
+  code = "&code=" + code;
+  req.open("GET", path + client_id + redirect_uri + client_secret + code);
+  req.send();
+}
+
+function gotAccessToken() {
+  let resp_obj = JSON.parse(this.response);
+  let tok = resp_obj['access_token'];
+  if (tok !== undefined) {
+    let req = new XMLHttpRequest();
+    req.open("POST", "https://graph.facebook.com/v2.7/me/live_videos?access_token=" + tok);
+    req.addEventListener('load', onLiveVideo);
+    req.send();
+  }
+}
+
+function onLiveVideo() {
+  let resp_obj = JSON.parse(this.response);
+  console.log(resp_obj);
+  let stream_url = resp_obj['stream_url'];
+  if (stream_url !== undefined) {
+    document.getElementById('url').value = stream_url;
+  }
+}
+
 function init() {
   document.getElementById('go').addEventListener('click', goOnClick);
+  let wv = document.getElementById('wv');
+  wv.addEventListener('contentload', wvOnInitialLoad);
+  wv.addEventListener('loadredirect', wvOnRedirect);
+  wv.src = "https://www.facebook.com/dialog/oauth?client_id=1072171686192806&scope=publish_actions&redirect_uri=https://www.facebook.com/connect/login_success.html";
 }
 
 // This function is called by common.js when the NaCl module is
